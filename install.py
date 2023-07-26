51b8c41d51b3d1066edd4b7a6d3762535b514608
Roman Kutanov <rk@alms.app>
2023-07-26 19:00:28 +0200

    fix setup

--- install.py
+++ install.py
@@ -2,3 +2,5 @@ import launch
 
 if not launch.is_installed("diffusers"):
     launch.run_pip(f"install diffusers", "diffusers")
+if not launch.is_installed("onnxruntime-gpu"):
+    launch.run_pip(f"install onnxruntime-gpu", "onnxruntime-gpu")
--- scripts/censor.py
+++ scripts/censor.py
@@ -5,13 +5,13 @@ import numpy as np
 import torch
 from PIL import Image
 from diffusers.utils import logging
-from scripts.safety_checker import StableDiffusionSafetyChecker
-from transformers import AutoFeatureExtractor
+# from scripts.safety_checker import StableDiffusionSafetyChecker
+# from transformers import AutoFeatureExtractor
 
 # fast nsfw
 from scripts.censor_model import model as onnx_model
 
-#from modules import scripts
+from modules import scripts
 
 logger = logging.get_logger(__name__)
 
@@ -34,30 +34,31 @@ def numpy_to_pil(images):
 
 
 # check and replace nsfw content
-def check_safety(x_image, safety_checker_adj: float):
-    global safety_feature_extractor, safety_checker
+# def check_safety(x_image, safety_checker_adj: float):
+#     global safety_feature_extractor, safety_checker
 
-    if safety_feature_extractor is None:
-        safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
-        safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
+#     if safety_feature_extractor is None:
+#         safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
+#         safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
 
-    safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
-    x_checked_image, has_nsfw_concept = safety_checker(
-        images=x_image,
-        clip_input=safety_checker_input.pixel_values,
-        safety_checker_adj=safety_checker_adj,  # customize adjustment
-    )
+#     safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
+#     x_checked_image, has_nsfw_concept = safety_checker(
+#         images=x_image,
+#         clip_input=safety_checker_input.pixel_values,
+#         safety_checker_adj=safety_checker_adj,  # customize adjustment
+#     )
 
-    return x_checked_image, has_nsfw_concept
+#     return x_checked_image, has_nsfw_concept
 
 
 def censor_batch(x, safety_checker_adj: float):
-    predictions = [onnx_model.predict(x_sample) for x_sample in x]
+    pil_images = numpy_to_pil(x)
+    predictions = [onnx_model.predict(x_sample) for x_sample in pil_images]
     index = 0
     for p in predictions:
         try:
-            safety = filter(lambda predicate: p['predictions'] predicate.label == 'Safety')['confidence']
-            naked = filter(lambda predicate: p['predictions'] predicate.label == 'Naked')['confidence']
+            safety = next(filter(lambda predicate: predicate.label == 'Safety',  p['predictions'])['confidence'])
+            naked = next(filter(lambda predicate: predicate.label == 'Naked', p['predictions'] )['confidence'])
             if safety > 0.7 + safety_checker_adj and naked > 0.5 + safety_checker_adj: 
                 hwc = x.shape
                 y = Image.open(warning_image).convert("RGB").resize((hwc[3], hwc[2]))
@@ -73,34 +74,34 @@ def censor_batch(x, safety_checker_adj: float):
     return x
 
 
-class NsfwCheckScript(scripts.Script):
-    def title(self):
-        return "NSFW check"
+# class NsfwCheckScript(scripts.Script):
+#     def title(self):
+#         return "NSFW check"
 
-    def show(self, is_img2img):
-        return scripts.AlwaysVisible
+#     def show(self, is_img2img):
+#         return scripts.AlwaysVisible
 
-    def postprocess_batch(self, p, *args, **kwargs):
-        """
-        Args:
-            p:
-            *args:
-                args[0]: enable_nsfw_filer. True: NSFW filter enabled; False: NSFW filter disabled
-                args[1]: safety_checker_adj
-            **kwargs:
-        Returns:
-            images
-        """
+#     def postprocess_batch(self, p, *args, **kwargs):
+#         """
+#         Args:
+#             p:
+#             *args:
+#                 args[0]: enable_nsfw_filer. True: NSFW filter enabled; False: NSFW filter disabled
+#                 args[1]: safety_checker_adj
+#             **kwargs:
+#         Returns:
+#             images
+#         """
 
-        images = kwargs['images']
-        if args[0] is True:
-            images[:] = censor_batch(images, args[1])[:]
+#         images = kwargs['images']
+#         if args[0] is True:
+#             images[:] = censor_batch(images, args[1])[:]
 
-    def ui(self, is_img2img):
-        enable_nsfw_filer = gr.Checkbox(label='Enable NSFW filter',
-                                        value=False,
-                                        elem_id=self.elem_id("enable_nsfw_filer"))
-        safety_checker_adj = gr.Slider(label="Safety checker adjustment",
-                                       minimum=-0.5, maximum=0.5, value=0.0, step=0.001,
-                                       elem_id=self.elem_id("safety_checker_adj"))
-        return [enable_nsfw_filer, safety_checker_adj]
+#     def ui(self, is_img2img):
+#         enable_nsfw_filer = gr.Checkbox(label='Enable NSFW filter',
+#                                         value=False,
+#                                         elem_id=self.elem_id("enable_nsfw_filer"))
+#         safety_checker_adj = gr.Slider(label="Safety checker adjustment",
+#                                        minimum=-0.5, maximum=0.5, value=0.0, step=0.001,
+#                                        elem_id=self.elem_id("safety_checker_adj"))
+#         return [enable_nsfw_filer, safety_checker_adj]
